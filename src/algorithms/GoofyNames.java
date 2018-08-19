53b39f335cf453217ce00bfe9eab8924f3cb3a42
league <league@wts10.attlocal.net>
2018-08-18 17:26:51 -0700

    GoofyNames

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -11,13 +11,15 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+	String mario =      	JOptionPane.showInputDialog("What is your name? ");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+			System.out.println(mario.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
-
+				  for (int i = 0; i < mario.length(); i++) {
+					String fortnite = mario.substring(i,i+1);
+				  }
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
@@ -31,6 +33,11 @@ public class GoofyNames {
 		
 		// 7. Use pop-up to show user their Goofy name
 
+	}
+
+	private static char[] toUpperCase() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
 
