39ad397fd2f519d5384afbb9156a137313e1fb4b
Rohan890 <league@wts7.attlocal.net>
2018-09-01 17:23:50 -0700

    GoofyNames

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -11,42 +11,44 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-	String mario =      	JOptionPane.showInputDialog("What is your name? ");
+		String mario = JOptionPane.showInputDialog("What is your name? ");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-			System.out.println(mario.toUpperCase());
+		// Run your program to see that this works.
+		System.out.println(mario.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
-				  for (int i = 0; i < mario.length(); i++) {
-					String fortnite = mario.substring(i,i+1);
-if (i%2 == 0  ) {goofyName = fortnite.toUpperCase();
-}
-
-else{ 
-
-}
-
-{
+		// HINT: Use .length() to determine the number of characters in the String.
+		for (int i = 0; i < mario.length(); i++) {
+			String fortnite = mario.substring(i, i + 1);
+			if (i % 2 == 0) {
+				goofyName = fortnite.toUpperCase();
 			}
+
+			else {
+
+			}
+
+			
+
+			
+			
+			goofyName = goofyName + fortnite;
 		}
-			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
+		JOptionPane.showMessageDialog(null, "Here is your goofy name:" + goofyName);  
+		// 4. Create a String variable to store the next character of the name
+		// using .substring(start, end)
+		// HINT: replace 'start' and 'end' to get String with character at i
 
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
+		// 5. Use MODULO to set this variable to upper case for EVEN characters
+		// and lower case for ODD characters.
 
-				  
+		// 6. ADD this String (containing 1 char) to the goofyName String
+
 		// 7. Use pop-up to show user their Goofy name
- 
+
 	}
 
 	private static char[] toUpperCase() {
 		// TODO Auto-generated method stub
-		return null; 
+		return null;
 	}
 }
-
