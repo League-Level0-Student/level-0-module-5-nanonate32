e5c309cb6b16aac4dc1f1c4f27e85f0f440f75b9
1902051 <league@wts22.attlocal.net>
2018-09-29 17:33:44 -0700

    circlering

--- src/nested_loops/CircleRing.java
+++ src/nested_loops/CircleRing.java
@@ -7,29 +7,40 @@ package nested_loops;
 import org.jointheleague.graphical.robot.Robot;
 
 public class CircleRing {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        // 1. Make a new Robot
+		// 1. Make a new Robot
+		Robot bobzilla = new Robot();
 
+		// 2. Set your robot’s position to x=150 and y=200
+		bobzilla.setX(150);
+		bobzilla.setY(200);
+		// 3. Put the robot's pen down
+		bobzilla.penDown();
+		// 4. Set the robot’s speed to 10
+		bobzilla.setSpeed(10000);
+		// 5. Do everything below here 360 times (use i as the counter)
+		for (int i = 0; i < 360; i++) {
+			bobzilla.move(3);
+			bobzilla.turn(1);
+			if (i % 20 == 0) {
+				for (int j = 0; j < 360; j++) {
+					bobzilla.move(1);
+					bobzilla.turn(1);
+				}
+			}
+		}
 
-       //  2. Set your robot’s position to x=150 and y=200
+		// 6. Move the robot 3 pixels
 
-        // 3. Put the robot's pen down
+		// 7. Turn the robot 1 degree
 
-        // 4. Set the robot’s speed to 10
-
-        // 5. Do everything below here 360 times (use i as the counter)
-
-	        // 6. Move the robot 3 pixels
-
-       //  7. Turn the robot 1 degree
-
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
+		// 8. If the counter i is divisible by 20 (hint: use mod operator %)..
 
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
 
-				// 10. Move the robot 1 pixel
+		// 10. Move the robot 1 pixel
 
-				// 11. Turn the robot 1 degree
-}
+		// 11. Turn the robot 1 degree
+	}
 }
