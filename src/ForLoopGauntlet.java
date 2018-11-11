ff6e4660fbee4f6c599d7f1324ba10bbf8e4816c
league <league@wts22.attlocal.net>
2018-11-10 17:23:38 -0800

    ForLoopGauntlet

--- src/ForLoopGauntlet.java
+++ src/ForLoopGauntlet.java
@@ -2,43 +2,58 @@
 public class ForLoopGauntlet {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 101; i++) {
-			System.out.println(" ");
-		}
-		for (int i = 101; i > -1; i--) {
-			System.out.println("");
-		}
-		for (int i = 2; i < 101; i++) {
-			if (i % 2 == 0) {
-				System.out.println(" ");
-			}
-		}
-		for (int i = 1; i < 100; i++) {
-			if (i % 2 == 1) {
-				System.out.println("");
-			}
-		}
-		for (int i = 1; i < 501; i++) {
-			if(i % 2 == 0) {
-				System.out.println("" + " ");}
-				if(i % 2 == 1) {
-					System.out.println("" + " ");
-				}
-		}
-		for (int i = 0; i < 778; i++) {
-		   if( i % 7 == 0) {
-			   System.out.println("");
-		}	
-		
-		
-		}
-		for (int i = 0; i < 13; i++) {
-		int Yearborn = i + 2006;
-			System.out.println("In the year " + Yearborn + " I was " + i + " years old" );
-		}
+		// for (int i = 0; i < 101; i++) {
+		// System.out.println(" ");
+		// }
+		// for (int i = 101; i > -1; i--) {
+		// System.out.println("");
+		// }
+		// for (int i = 2; i < 101; i++) {
+		// if (i % 2 == 0) {
+		// System.out.println(" ");
+		// }
+		// }
+		// for (int i = 1; i < 100; i++) {
+		// if (i % 2 == 1) {
+		// System.out.println("");
+		// }
+		// }
+		// for (int i = 1; i < 501; i++) {
+		// if (i % 2 == 0) {
+		// System.out.println("" + " ");
+		// }
+		// if (i % 2 == 1) {
+		// System.out.println("" + " ");
+		// }
+		// }
+		// for (int i = 0; i < 778; i++) {
+		// if (i % 7 == 0) {
+		// System.out.println("");
+		// }
+		//
+		// }
+		// for (int i = 0; i < 13; i++) {
+		// int Yearborn = i + 2006;
+		// System.out.println("In the year " + "Yearborn" + " I was " + "i" + " ");
+		// }
+		// for (int i = 0; i < 3; i++) {
+		// for (int j = 0; j < 3; j++) {
+		// System.out.println(i + " " + j);
+		// }
+		// for (int i = 0; i < 9; i += 3) {
+		// for (int j = 1; j < 4; j++) {
+		// System.out.print(i + j + " ");
+		// }
+		// System.out.println();
+
+		// for (int i = 0; i < 100; i += 10) {
+		// for (int j = 1; j < 11; j++) {
+		// System.out.print(i + j + " ");
+		// }
+		// System.out.println();
+		// }
 		for (int i = 0; i < args.length; i++) {
-			
+
 		}
 	}
-
 }
