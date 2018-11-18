00d6e91de0c8aff02b216497c7b693fd2e70bcc2
League <League@adminisorsimac2.attlocal.net>
2018-11-18 12:55:12 -0800

    forloopgauntlet

--- src/ForLoopGauntlet.java
+++ src/ForLoopGauntlet.java
@@ -52,8 +52,15 @@ public class ForLoopGauntlet {
 		// }
 		// System.out.println();
 		// }
-		for (int i = 0; i < args.length; i++) {
+		for (int i = 0; i < 6; i++) {
+		for (int j = 0; j <= i; j++) {
+			System.out.print(" * ");
+		}
+		System.out.println();
+		}
+	
+		}
 
 		}
-	}
-}
+	
+
